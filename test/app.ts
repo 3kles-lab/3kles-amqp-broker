ef060cc9ce11f12a1adb22793c76df2dc1e31701
Charles Da Silva Costa <cdasilvacosta@3kles-consulting.com>
2021-10-19 18:51:45 +0200

    Change version

--- test/app.ts
+++ test/app.ts
@@ -11,26 +11,26 @@ import { MessageBroker } from '../src/message-broker';
     const broker = await MessageBroker.getInstance();
 
     /*Queue*/
-    await broker.subscribe('test.queue', async (msg, ack) => {
+    await broker.subscribe('ceciestuntest', async (msg, ack) => {
         console.log('Message from queue:', msg.content.toString());
         ack();
     });
 
-    await broker.send('test.queue', Buffer.from('hello from queue'));
+    // await broker.send('test.tata', Buffer.from('hello from queue'));
     /* ****/
 
     /*Exchange*/
-    await broker.subscribeExchange('', 'test.exchange', 'test.routingKey', 'direct', async (msg, ack) => {
-        console.log('Message from exchange :', msg.content.toString());
-        ack();
-    });
+    // await broker.subscribeExchange('', 'test.exchange', 'test.routingKey', 'direct', async (msg, ack) => {
+    //     console.log('Message from exchange :', msg.content.toString());
+    //     ack();
+    // });
 
-    await broker.sendToExchange('test.exchange', 'test.routingKey', Buffer.from('hello from exchange'));
+    // await broker.sendToExchange('test.exchange', 'test.routingKey', Buffer.from('hello from exchange'));
     /* ***/
 
     /*Send RPC message and wait response*/
-    const response = await broker.sendRPCMessage('m3api', Buffer.from(JSON.stringify({ api: 'MNS150MI', transaction: 'GetUserData' })));
+    // const response = await broker.sendRPCMessage('m3api', Buffer.from(JSON.stringify({ api: 'MNS150MI', transaction: 'GetUserData' })));
 
-    await broker.disconnect();
+    // await broker.disconnect();
 
 })();
