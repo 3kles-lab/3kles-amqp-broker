bddfa81ca57d714e5151ff619cd649e7c885c4a0
Charles Da Silva Costa <CDASILVA@3KLES.local>
2023-02-02 17:56:03 +0100

    Force exit on error with listen rpc

--- src/message-broker.ts
+++ src/message-broker.ts
@@ -343,18 +343,23 @@ export class MessageBroker {
     }
 
     private async listenRPC(): Promise<void> {
-        await this.channel.consume(this.rpcQueue.queue, (msg) => {
-            if (msg) {
-                const index = this.correlationIds.indexOf(msg.properties.correlationId, 0);
-                if (index !== -1) {
-                    this.responseEmitter.emit(
-                        msg.properties.correlationId,
-                        JSON.parse(msg.content.toString('utf8')),
-                    );
-                    this.correlationIds.splice(index, 1);
+        try {
+            await this.channel.consume(this.rpcQueue.queue, (msg) => {
+                if (msg) {
+                    const index = this.correlationIds.indexOf(msg.properties.correlationId, 0);
+                    if (index !== -1) {
+                        this.responseEmitter.emit(
+                            msg.properties.correlationId,
+                            JSON.parse(msg.content.toString('utf8')),
+                        );
+                        this.correlationIds.splice(index, 1);
+                    }
                 }
-            }
-        }, { noAck: true });
-
+            }, { noAck: true });
+        } catch (err) {
+            console.error('[AMQP] Error on listen rpc');
+            console.error(err);
+            process.exit();
+        }
     }
 }
